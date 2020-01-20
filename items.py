d4032dea443f13d5a1f7a5ca2ed323aa4044006b
olhaexe <53696639+olhaexe@users.noreply.github.com>
2020-01-20 18:29:31 +0300

    Update items.py

--- items.py
+++ items.py
@@ -8,27 +8,6 @@
 import scrapy
 from scrapy.loader.processors import MapCompose, TakeFirst
 
-def cleaner_photo(values):
-    if values[:2] == '//':
-        return f'https:{values}'
-    return values
-
-def cleaner_params(item: str):
-    result = item.split('">')[-1].split(':')
-    key = result[0]
-    value = result[-1].split('</span>')[-1].split('</')[0][:-1]
-    try:
-        value = int(value)
-    except ValueError:
-        pass
-    return {key: value}
-
-def dict_params(items):
-    result = {}
-    for item in items:
-        result.update(item)
-    return result
-
 def title_string(words):
     string = ''
     for word in words:
@@ -40,15 +19,6 @@ def salary_string(words):
     for word in words:
         string += word
     return string
-
-class AvitoparseItem(scrapy.Item): # словарьсовместимый, используем как обычный словарь
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    _id = scrapy.Field()
-    url = scrapy.Field(output_processor=TakeFirst())
-    photos = scrapy.Field(input_processor=MapCompose(cleaner_photo))
-    title = scrapy.Field(output_processor=TakeFirst())
-    params = scrapy.Field(input_processor=MapCompose(cleaner_params), output_processor=dict_params)
 
 class HeadhunterItem(scrapy.Item):
     _id = scrapy.Field()
