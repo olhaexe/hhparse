c6dc7418fd1740fee646a9d60d7ab092f0cf7cb0
olhaexe <53696639+olhaexe@users.noreply.github.com>
2020-01-22 12:05:06 +0300

    Update items.py

--- items.py
+++ items.py
@@ -14,17 +14,11 @@ def title_string(words):
         string += word
     return string
 
-def salary_string(words):
-    string = ''
-    for word in words:
-        string += word
-    return string
-
 class HeadhunterItem(scrapy.Item):
     _id = scrapy.Field()
     title = scrapy.Field(input_processor=MapCompose(title_string), output_processor=TakeFirst())
     url = scrapy.Field(output_processor=TakeFirst())
-    salary = scrapy.Field(input_processor=MapCompose(title_string), output_processor=salary_string)
+    salary = scrapy.Field(input_processor=MapCompose(title_string), output_processor=title_string)
     skills = scrapy.Field()
     enterprise = scrapy.Field(output_processor=TakeFirst())
     enterprise_url = scrapy.Field(output_processor=TakeFirst())
